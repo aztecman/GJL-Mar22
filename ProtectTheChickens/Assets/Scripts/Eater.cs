8e0420b19cc31386bb80eeef84f58e1afbc3ecc3
aztecman <aztecmanmusic@gmail.com>
2022-03-20 11:41:00 -0700

    Added Self collision and tail detection
    snake can eat its own body (death) or eat its tail (win, debugging for now)

--- ProtectTheChickens/Assets/Scripts/Eater.cs
+++ ProtectTheChickens/Assets/Scripts/Eater.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Eater : MonoBehaviour
 {
+    [SerializeField] bool isSnek = false;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     public int GetConsumableFromMouth(Vector3 mouthPosition, GameObject hungryObject)
     {
-        Collider[] hitColliders = Physics.OverlapSphere(mouthPosition, .2f);
+        Collider[] hitColliders = Physics.OverlapSphere(mouthPosition, .05f);
         foreach (var hitCollider in hitColliders)
         {
             if (hitCollider.CompareTag("Consumable"))
@@ -25,6 +33,24 @@ public class Eater : MonoBehaviour
                     Debug.Log("Yum! Value: " + foodValue);
                     //TODO: show the player message: YUM, or BLEH
                     return foodValue;
+                }
+            }
+            if (isSnek)
+            {
+                if (hitCollider.CompareTag("Snek"))
+                {
+                    if (!ReferenceEquals(hitCollider.gameObject, hungryObject))
+                    {
+                        //Debug.Log(mouthPosition);
+                        //Debug.Log(hitCollider.name);
+                        //Debug.Log(hitCollider.transform.position);
+                        gameManager.PlayerDeath();
+                        return 0;
+                    }
+                }
+                if (hitCollider.CompareTag("SnekTail"))
+                {
+                    Debug.Log("Connection!");
                 }
             }
         }
